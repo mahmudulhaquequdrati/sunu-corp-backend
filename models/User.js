3e2c889b1ba306498c5b1fd08750466dfd1f0c49
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2022-12-11 20:16:05 +0600

    upated

--- models/User.js
+++ models/User.js
@@ -13,6 +13,34 @@ const userSchema = mongoose.Schema(
       type: Number,
       required: true,
     },
+    date_of_birth: {
+      type: Number,
+      required: true,
+    },
+    place_of_birth: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    year_of_degree: {
+      type: Number,
+      required: true,
+    },
+    major_of_degree: {
+      type: String,
+      required: true,
+    },
+    reason_for_request: {
+      type: String,
+      required: true,
+    },
+    payment_confirmation: {
+      type: String,
+      required: true,
+    },
   },
   {
     timestamps: true,
